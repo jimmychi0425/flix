02e15819acee3f548301ff10a8efec7b91d3bd99
Han Chi <hanchi@HandeMacBook-Pro.local>
2018-02-03 18:49:12 -0800

    add the feature of pulling to fresh

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # flix
-# Project 1 - *Name of App Here*
+# Project 1 - *flix*
 
-**Name of your app** is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+**flix** is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
 
 Time spent: **X** hours spent in total
 
@@ -11,7 +11,7 @@ The following **required** user stories are complete:
 
 - [X] User sees app icon in home screen and styled launch screen (+1pt)
 - [X] User can scroll through a list of movies currently playing in theaters from The Movie DB API (+5pt)
-- [ ] User can "Pull to refresh" the movie list (+2pt)
+- [X] User can "Pull to refresh" the movie list (+2pt)
 - [ ] User sees a loading state while waiting for the movies to load (+2pt)
 
 The following **stretch** user stories are implemented:
--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -12,14 +12,24 @@ import AlamofireImage
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     var movies: [[String : Any]] = []
+    var refreshControl: UIRefreshControl!
     @IBOutlet weak var tableView: UITableView!
   
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
         self.tableView.rowHeight = 200
-        
+        fetchMovies()
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -32,6 +42,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String : Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
         task.resume()
