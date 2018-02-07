953e2de169e37faa1f760d65c2d0dbfe02830040
Han Chi <hanchi@HandeMacBook-Pro.local>
2018-02-06 21:07:44 -0800

    Collection View Setup

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Han Chi on 2018/2/6.
+//  Copyright © 2018年 Han Chi. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
